eaad205cc36189efc0f0c1c617cb9ee197f8ebde
Kunal Kankariya <kkankariya007@gmail.com>
2023-10-14 09:29:05 +0530

    Updates to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+
 
 void main() {
 
@@ -45,4 +48,34 @@ class _CameraScreenState extends State<CameraScreen> {
       ),
     );
   }
+}
+
+Future upload() async{
+
+  try {
+    var request = http.MultipartRequest(
+        "POST", Uri.parse("https://anizam.up.railway.app/name/"));
+    // var audio = await http.MultipartFile.fromBytes('file',
+    //     await File.fromUri(Uri.parse("/storage/emulated/0/Download/Anizam/temp.wav")).readAsBytes(),
+        filename: 'temp.wav',
+        contentType: MediaType.parse('audio/wav')//'audio', 'wav')
+    );
+
+    request.files.add(audio);
+    var response = await request.send();
+    var responseData = await response.stream.toBytes();
+    var result = String.fromCharCodes(responseData);
+
+    String predict=result.toString();
+    int idx=predict.indexOf('":"');
+    charac=predict.substring(2,idx);
+    anime=predict.substring(idx+3,predict.length-2);
+
+  }
+  catch(e)
+  {
+    // print(e);
+  }
+  // _hasp=true;
+
 }
